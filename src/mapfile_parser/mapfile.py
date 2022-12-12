ac647dd9291b59ba4a55900de9b07b55f438ae47
Angie <angheloalf95@gmail.com>
2022-12-12 16:38:55 -0300

    MapFile class

--- src/mapfile_parser/mapfile.py
+++ src/mapfile_parser/mapfile.py
@@ -8,6 +8,7 @@ from __future__ import annotations
 import dataclasses
 import re
 import collections
+from pathlib import Path
 
 
 regex_fileDataEntry = re.compile(r"^\s+(?P<section>[^\s]+)\s+(?P<vram>0x[^\s]+)\s+(?P<size>0x[^\s]+)\s+(?P<name>[^\s]+)$")
@@ -27,105 +28,104 @@ class File:
     size: int
     functions: list[Function]
 
+class MapFile:
+    def __init__(self, mapPath: Path, startingPoint: str="\n build/"):
+        self.filesList: list[File] = list()
 
-def parseMapFile(mapPath: str, startingPoint: str) -> list[File]:
-    with open(mapPath) as f:
-        mapData = f.read()
-        startIndex = mapData.find(startingPoint)
-        mapData = mapData[startIndex:]
-    # print(len(mapData))
+        with mapPath.open() as f:
+            mapData = f.read()
+            startIndex = mapData.find(startingPoint)
+            mapData = mapData[startIndex:]
+        # print(len(mapData))
 
-    filesList: list[File] = list()
+        inFile = False
 
-    inFile = False
+        mapLines = mapData.split("\n")
+        for line in mapLines:
+            if inFile:
+                if line.startswith("                "):
+                    entryMatch = regex_functionEntry.search(line)
 
-    mapLines = mapData.split("\n")
-    for line in mapLines:
-        if inFile:
-            if line.startswith("                "):
-                entryMatch = regex_functionEntry.search(line)
+                    # Find function
+                    if entryMatch is not None:
+                        funcName = entryMatch["name"]
+                        funcVram = int(entryMatch["vram"], 16)
 
-                # Find function
-                if entryMatch is not None:
-                    funcName = entryMatch["name"]
-                    funcVram = int(entryMatch["vram"], 16)
+                        # Filter out jump table's labels
+                        labelMatch = regex_label.search(funcName)
+                        if labelMatch is None:
+                            self.filesList[-1].functions.append(Function(funcName, funcVram, -1))
+                        # print(hex(funcVram), funcName)
 
-                    # Filter out jump table's labels
-                    labelMatch = regex_label.search(funcName)
-                    if labelMatch is None:
-                        filesList[-1].functions.append(Function(funcName, funcVram, -1))
-                    # print(hex(funcVram), funcName)
-
+                else:
+                    inFile = False
             else:
-                inFile = False
-        else:
-            if line.startswith(" .text "):
-                inFile = False
-                entryMatch = regex_fileDataEntry.search(line)
+                if line.startswith(" .text "):
+                    inFile = False
+                    entryMatch = regex_fileDataEntry.search(line)
 
-                # Find file
-                if entryMatch is not None:
-                    name = "/".join(entryMatch["name"].split("/")[2:])
-                    name = ".".join(name.split(".")[:-1])
-                    size = int(entryMatch["size"], 16) // 4
-                    vram = int(entryMatch["vram"], 16)
+                    # Find file
+                    if entryMatch is not None:
+                        name = "/".join(entryMatch["name"].split("/")[2:])
+                        name = ".".join(name.split(".")[:-1])
+                        size = int(entryMatch["size"], 16) // 4
+                        vram = int(entryMatch["vram"], 16)
 
-                    if size > 0:
-                        inFile = True
-                        filesList.append(File(name, vram, size, list()))
+                        if size > 0:
+                            inFile = True
+                            self.filesList.append(File(name, vram, size, list()))
 
-    resultFileList: list[File] = list()
+        resultFileList: list[File] = list()
 
-    for file in filesList:
-        acummulatedSize = 0
-        funcCount = len(file.functions)
+        for file in self.filesList:
+            acummulatedSize = 0
+            funcCount = len(file.functions)
 
-        # Filter out files with no functions
-        if funcCount == 0:
-            continue
-
-        # Calculate size of each function
-        for index in range(funcCount-1):
-            func = file.functions[index]
-            nextFunc = file.functions[index+1]
-
-            size = (nextFunc.vram - func.vram) // 4
-            acummulatedSize += size
-
-            file.functions[index] = Function(func.name, func.vram, size)
-
-        # Calculate size of last function of the file
-        func = file.functions[funcCount-1]
-        size = file.size - acummulatedSize
-        file.functions[funcCount-1] = Function(func.name, func.vram, size)
-
-        resultFileList.append(file)
-
-    return resultFileList
-
-
-def removeDottedSymbols(filesList: list[File]) -> list[File]:
-    resultFileList: list[File] = list()
-
-    for file in filesList:
-        newFile = File(file.name, file.vram, file.size, list())
-
-        lastFunc: Function|None = None
-
-        for func in file.functions:
-            newFunc = Function(func.name, func.vram, func.size)
-
-            if func.name.startswith("."):
-                assert lastFunc is not None, file
-                lastFunc.size += func.size
+            # Filter out files with no functions
+            if funcCount == 0:
                 continue
 
-            lastFunc = newFunc
-            newFile.functions.append(newFunc)
+            # Calculate size of each function
+            for index in range(funcCount-1):
+                func = file.functions[index]
+                nextFunc = file.functions[index+1]
 
-        resultFileList.append(newFile)
+                size = (nextFunc.vram - func.vram) // 4
+                acummulatedSize += size
 
-    return resultFileList
+                file.functions[index] = Function(func.name, func.vram, size)
+
+            # Calculate size of last function of the file
+            func = file.functions[funcCount-1]
+            size = file.size - acummulatedSize
+            file.functions[funcCount-1] = Function(func.name, func.vram, size)
+
+            resultFileList.append(file)
+
+
+
+    def removeDottedSymbols(self):
+        resultFileList: list[File] = list()
+
+        for file in self.filesList:
+            newFile = File(file.name, file.vram, file.size, list())
+
+            lastFunc: Function|None = None
+
+            for func in file.functions:
+                newFunc = Function(func.name, func.vram, func.size)
+
+                if func.name.startswith("."):
+                    assert lastFunc is not None, file
+                    lastFunc.size += func.size
+                    continue
+
+                lastFunc = newFunc
+                newFile.functions.append(newFunc)
+
+            resultFileList.append(newFile)
+
+        self.filesList = resultFileList
 
 
 def mixFolders(filesList: list[File]) -> list[File]:
