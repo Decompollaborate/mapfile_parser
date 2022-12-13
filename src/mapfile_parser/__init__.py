f7a6eb8c5616d41b7571a6580cb8c2d8f107069c
Angie <angheloalf95@gmail.com>
2022-12-12 22:21:44 -0300

    Add version string

--- src/mapfile_parser/__init__.py
+++ src/mapfile_parser/__init__.py
@@ -5,6 +5,9 @@
 
 from __future__ import annotations
 
+__version_info__ = (0, 0, 1)
+__version__ = ".".join(map(str, __version_info__)) + "-dev"
+__author__ = "Decompollaborate"
 
 from .mapfile import MapFile
 from .mapfile import Symbol, File
