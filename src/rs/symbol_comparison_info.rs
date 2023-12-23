978b74b7de416a53a634605434f87e553c247445
angie <angheloalf95@gmail.com>
2023-12-23 11:28:50 -0300

    MapsComparisonInfo and SymbolComparisonInfo

--- src/rs/symbol_comparison_info.rs
+++ src/rs/symbol_comparison_info.rs
@@ -2,34 +2,27 @@
 /* SPDX-License-Identifier: MIT */
 
 use crate::{file, symbol};
+
+#[cfg(feature = "python_bindings")]
 use pyo3::prelude::*;
 
 #[derive(Debug, Clone)]
-#[pyclass(module = "mapfile_parser")]
+#[cfg_attr(feature = "python_bindings", pyclass(module = "mapfile_parser"))]
 pub struct SymbolComparisonInfo {
-    #[pyo3(get, set)]
     pub symbol: symbol::Symbol,
 
-    #[pyo3(get, set, name = "buildAddress")]
     pub build_address: u64,
 
-    #[pyo3(get, set, name = "buildFile")]
     pub build_file: Option<file::File>,
 
-    #[pyo3(get, set, name = "expectedAddress")]
     pub expected_address: u64,
 
-    #[pyo3(get, set, name = "expectedFile")]
     pub expected_file: Option<file::File>,
 
-    #[pyo3(get, set)]
     pub diff: Option<i64>,
 }
 
-#[pymethods]
 impl SymbolComparisonInfo {
-    #[new]
-    #[pyo3(signature = (symbol, build_address, build_file, expected_address, expected_file, diff))]
     pub fn new(
         symbol: symbol::Symbol,
         build_address: u64,
@@ -38,13 +31,108 @@ impl SymbolComparisonInfo {
         expected_file: Option<file::File>,
         diff: Option<i64>,
     ) -> Self {
-        SymbolComparisonInfo {
+        Self {
             symbol,
             build_address,
             build_file,
             expected_address,
             expected_file,
             diff,
+        }
+    }
+}
+
+#[cfg(feature = "python_bindings")]
+#[allow(non_snake_case)]
+pub(crate) mod python_bindings {
+    use pyo3::prelude::*;
+
+    use crate::{file, symbol};
+
+    #[pymethods]
+    impl super::SymbolComparisonInfo {
+        #[new]
+        #[pyo3(signature = (symbol, build_address, build_file, expected_address, expected_file, diff))]
+        pub fn py_new(
+            symbol: symbol::Symbol,
+            build_address: u64,
+            build_file: Option<file::File>,
+            expected_address: u64,
+            expected_file: Option<file::File>,
+            diff: Option<i64>,
+        ) -> Self {
+            Self::new (
+                symbol,
+                build_address,
+                build_file,
+                expected_address,
+                expected_file,
+                diff,
+            )
+        }
+
+        /* Getters and setters */
+
+        #[getter]
+        fn get_symbol(&self) -> PyResult<symbol::Symbol> {
+            Ok(self.symbol.clone())
+        }
+        #[setter]
+        fn set_symbol(&mut self, value: symbol::Symbol) -> PyResult<()> {
+            self.symbol = value;
+            Ok(())
+        }
+
+        #[getter]
+        fn get_buildAddress(&self) -> PyResult<u64> {
+            Ok(self.build_address)
+        }
+        #[setter]
+        fn set_buildAddress(&mut self, value: u64) -> PyResult<()> {
+            self.build_address = value;
+            Ok(())
+        }
+
+        #[getter]
+        fn get_buildFile(&self) -> PyResult<Option<file::File>> {
+            Ok(self.build_file.clone())
+        }
+        #[setter]
+        fn set_buildFile(&mut self, value: Option<file::File>) -> PyResult<()> {
+            self.build_file = value;
+            Ok(())
+        }
+
+        #[getter]
+        fn get_expectedAddress(&self) -> PyResult<u64> {
+            Ok(self.expected_address.clone())
+        }
+        #[setter]
+        fn set_expectedAddress(&mut self, value: u64) -> PyResult<()> {
+            self.expected_address = value;
+            Ok(())
+        }
+
+        #[getter]
+        fn get_expectedFile(&self) -> PyResult<Option<file::File>> {
+            Ok(self.expected_file.clone())
+        }
+
+        #[setter]
+        fn set_expectedFile(&mut self, value: Option<file::File>) -> PyResult<()> {
+            self.expected_file = value;
+            Ok(())
+        }
+
+        #[getter]
+        fn get_diff(&self) -> PyResult<Option<i64>> {
+            Ok(self.diff.clone())
+        }
+
+        #[setter]
+        fn set_diff(&mut self, value: Option<i64>) -> PyResult<()> {
+            self.diff = value;
+            Ok(())
         }
     }
 }
