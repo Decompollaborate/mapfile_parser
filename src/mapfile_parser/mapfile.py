4046e0e993ce4f20e1c8eae5ddfcc82e847f26d9
Anghelo Carvajal <angheloalf95@gmail.com>
2026-01-17 10:24:49 -0300

    changelog

--- src/mapfile_parser/mapfile.py
+++ src/mapfile_parser/mapfile.py
@@ -1168,8 +1168,7 @@ class MapFile:
                 # Only read the map if this is a new plf.
                 if sect.filepath in knownMaps:
                     continue
-                other_map_path = resolver(sect.filepath)
-                if other_map_path is not None:
+                if (other_map_path := resolver(sect.filepath)) is not None:
                     knownMaps[sect.filepath] = MapFile.newFromMapFile(other_map_path)
         return self._resolve_plf_impl(knownMaps)
 
@@ -1182,8 +1181,7 @@ class MapFile:
             newSeg = seg.cloneNoSectionlist()
 
             for sect in seg._sectionsList:
-                otherMap = knownMaps.get(sect.filepath)
-                if otherMap is not None:
+                if (otherMap := knownMaps.get(sect.filepath)) is not None:
                     # Each segment of a plf is just a normal elf section
                     partialSegment = None
                     for x in otherMap._segmentsList:
